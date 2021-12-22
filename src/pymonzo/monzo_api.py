7c120afe39f13704e630451bffac6f8966647d69
willscottuk <willscottuk@users.noreply.github.com>
2021-12-22 18:15:09 +0000

    dumps

--- src/pymonzo/monzo_api.py
+++ src/pymonzo/monzo_api.py
@@ -405,7 +405,7 @@ class MonzoAPI(CommonMixin):
         endpoint = '/transactions'
         response = self._get_response(
             method='get', endpoint=endpoint,
-            params=get_params,
+            params=json.dumps(get_params)),
         )
 
         transactions = response.json()['transactions']
